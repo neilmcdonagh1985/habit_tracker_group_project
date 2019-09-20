3d86d42c70b221f858a6a5c184b9115620dc1507
Ahmed Hussein <ahmd.adl.elsayed@gmail.com>
2019-09-20 15:40:29 +0100

    setup basic vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <add-meal-form />
+    </nav>
+    <meals-dashboard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddMealForm from './components/AddMealForm.vue';
+import MealsDashboard from './components/MealsDashboard.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'add-meal-form': AddMealForm,
+    'meals-dashboard': MealsDashboard 
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ client/src/components/AddMealForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>hello from add meal form</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: ''
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
