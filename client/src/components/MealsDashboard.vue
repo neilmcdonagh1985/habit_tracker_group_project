90fca88b36cf939b289065fdea0a226fb4ba375f
grahamsawers <grahamsawers@gmail.com>
2019-09-23 15:41:52 +0100

    Add apex-charts.

--- client/src/components/MealsDashboard.vue
+++ client/src/components/MealsDashboard.vue
@@ -1,35 +1,38 @@
 <template>
   <div>
-      <h1>Today's Total Calories: {{ todaysTotalCalories }}</h1>
-      <ul v-for="meal in todaysMeals" :key="meal._id">
-        <li>Your {{ meal.type }} was {{ meal.calories }} kcal</li>
-      </ul>
+    <daily-chart />
+    <ul v-for="meal in todaysMeals" :key="meal._id">
+      <li>Your {{ meal.type }} was {{ meal.calories }} kcal</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import DailyChart from '@/components/DailyChart.vue';
 export default {
   name: 'meals-dashboard',
   props: ['meals'],
+  components: {
+    DailyChart
+  },
   data() {
     return {
-      date: "2019-09-16"
-    }
+      date: '2019-09-16'
+    };
   },
   computed: {
     todaysMeals: function() {
-       return this.meals.filter(meal => meal.date === this.date)
-      },
-      todaysTotalCalories () {
-        return this.todaysMeals.map(el => el.calories)
-        .reduce((totalCalories, calories) => totalCalories + calories, 0)
-      },
+      return this.meals.filter(meal => meal.date === this.date);
+    },
+    todaysTotalCalories() {
+      return this.todaysMeals
+        .map(el => el.calories)
+        .reduce((totalCalories, calories) => totalCalories + calories, 0);
+    }
 
-      // (accumulator, currentValue) => accumulator + currentValue;
-  } 
-
-}
-
+    // (accumulator, currentValue) => accumulator + currentValue;
+  }
+};
 </script>
 
 <style>
